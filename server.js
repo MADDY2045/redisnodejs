1132d793dff934133b8455f9d278f38eed0a1cf2
madhavan <madhavaneee08@gmail.com>
2020-08-21 12:46:06 +0530

    f

--- server.js
+++ server.js
@@ -1,55 +1,20 @@
+const cpus = require('os').cpus();
 const express = require('express');
 const app = express();
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
-const cors = require('cors');
-
-app.use(cors());
-app.use(express.json());
+const cluster = require('cluster');
 const port = 6070;
 
-const oauth2Client = new OAuth2(
-    "485008776010-gqcbhhonfdefituo7pr206sqpmdl4ql6.apps.googleusercontent.com",
-    "tLyJNAGybUV33JZvlpaLMSU2",
-    "https://developers.google.com/oauthplayground"
-);
 
-oauth2Client.setCredentials({
-    refresh_token: "1//04v_p73yVXSMZCgYIARAAGAQSNwF-L9Ir0KBmspuZRcxLIDHTrnqKTbK8o6HC1oo-rrSLlhjDtOnnK5Ys_PoqhnCx2doCRcITzsY"
-});
-const accessToken = oauth2Client.getAccessToken()
+if(cluster.isMaster){
+    console.log(`this is a master process : ${process.pid}`);
+    for (let i=0;i<cpus.length;i++){
+        cluster.fork();
+    }
+}else{
 
-const smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-         type: "OAuth2",
-         user: "madhavaneee08@gmail.com",
-         clientId: "485008776010-gqcbhhonfdefituo7pr206sqpmdl4ql6.apps.googleusercontent.com",
-         clientSecret: "tLyJNAGybUV33JZvlpaLMSU2",
-         refreshToken: "1//04v_p73yVXSMZCgYIARAAGAQSNwF-L9Ir0KBmspuZRcxLIDHTrnqKTbK8o6HC1oo-rrSLlhjDtOnnK5Ys_PoqhnCx2doCRcITzsY",
-         accessToken: accessToken
-    },
-    tls: {
-        rejectUnauthorized: false
-      }
-});
-
-const mailOptions = {
-    from: "madhavaneee08@gmail.com",
-    to: "madhavan@growsmartsmb.com",
-    subject: "Another Test for oAuth",
-    generateTextFromHTML: true,
-    html: `<b>test</b><br><h3>Hello , good to here from you</h3>`
-};
-
-
-app.get('/',(req,res)=>{
-    smtpTransport.sendMail(mailOptions, (error, response) => {
-        error ? console.log(error) : console.log(response);
-        smtpTransport.close();
-    });
-    res.send("received");
-})
-
-app.listen(port,()=>console.log(`app is listening on port ${port}`));
+    app.get('/',(req,res)=>{
+        console.log(`worker pids are ${process.pid}`);
+       res.send(`Process pid is:${process.pid}`)
+    })
+    app.listen(port,()=>console.log(`app is listening on port ${port}`));
+}
